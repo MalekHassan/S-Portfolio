54f14f1f23020ea134169f7a6e6b9cfff6dae01f
MalekHassan <malekdalalha1995@gmail.com>
2020-08-27 14:43:14 +0300

    doing class 05

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const express = require ('express');
+
+
+const server = express();
+
+const PORT = 3000;
+
+server.get('/data',(request,response) => {
+    response.send('Hello World')})
+
+//  server.get('/club',(request,response) => {
+//      let club=[
+//          {name:'liverpool'},
+//          {name:'Real Madrid'},
+//          {name:'Bayern munchen'}
+//      ];
+//      response.json(club);
+//  })
+
+ server.listen(PORT,() => {
+    console.log(`lisining to my port!!! ${PORT}`);
+    })
